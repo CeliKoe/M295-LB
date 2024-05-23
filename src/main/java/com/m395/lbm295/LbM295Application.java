e8c1cf3ec1c6ca57d4226b3cbeb638b4eea4d676
celine <koelblc@bzz.ch>
2024-05-23 09:22:07 +0200

    second commmit

---
+++ src/main/java/com/m395/lbm295/LbM295Application.java
@@ -0,0 +1,13 @@
+package com.m395.lbm295;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbM295Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbM295Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/m395/lbm295/LbM295ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m395.lbm295;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbM295ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
